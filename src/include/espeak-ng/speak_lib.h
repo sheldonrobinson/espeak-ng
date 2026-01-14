38572d305c7c357789733da0cb7d9ee685a7ceb8
Sheldon Robinson <sheldon.robinson@live.com>
2026-01-14 13:37:18 -0500

    Update ESPEAK_API definition for DLL export/import

--- src/include/espeak-ng/speak_lib.h
+++ src/include/espeak-ng/speak_lib.h
@@ -29,10 +29,14 @@
 #include <stddef.h>
 
 #if defined(_WIN32) || defined(_WIN64)
-#ifdef LIBESPEAK_NG_EXPORT
-#define ESPEAK_API __declspec(dllexport)
+#ifdef LIBESPEAK_NG_DLL
+	#ifdef LIBESPEAK_NG_EXPORT
+		#define ESPEAK_API __declspec(dllexport)
+	#else
+		#define ESPEAK_API __declspec(dllimport)
+	#endif
 #else
-#define ESPEAK_API __declspec(dllimport)
+	#define ESPEAK_API
 #endif
 #else
 #define ESPEAK_API
